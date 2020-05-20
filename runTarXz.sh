b02ff886801948c0c9bc2e84c6f195795ceed647
Kartik-jalal <52451852+Kartik-jalal@users.noreply.github.com>
2020-05-20 16:09:32 +0000

    Add files via upload

---
+++ runTarXz.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+# to run tar.xz files
+
+
+echo "Please enter the file name(including the extension .xz):"
+read filename
+echo "Please tell me, where you want to extract it:"
+read directory
+
+if [-d $directory]
+then
+sudo tar -xvf $filename -C $directory
+else
+sudo mkdir $directory
+sudo tar -xvf $filename -C $directory
+fi
+echo "done" 
